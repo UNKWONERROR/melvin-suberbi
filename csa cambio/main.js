d1d264bf6ab6e00aa0163c35c7c48109abc4b709
UNKWONERROR <163881207+UNKWONERROR@users.noreply.github.com>
2024-03-18 11:19:20 -0400

    Add files via upload

---
+++ csa cambio/main.js
@@ -0,0 +1,38 @@
+
+function dolar() {
+    let valordolar=59;
+    var dePesoADolar = document.getElementById("cambio").value;
+    let resultado="";
+    resultado=dePesoADolar / valordolar
+
+     alert(resultado)
+}
+dolar();
+function peso() {
+    let valor2=59;
+    var deDolarAPeso  = document.getElementById("cambio").value;
+    let resultado="";
+    resultado=valor2 * deDolarAPeso
+
+     alert(resultado)
+}
+peso();
+function euro() {
+    let valor3=65;
+    var dePesoAEuro = document.getElementById("cambio").value;
+    let resultado="";
+    resultado=valor3 / dePesoAEuro
+    alert(resultado)
+}
+euro();
+function libra() {
+    let valor4=76
+    var dePesoAlibra = document.getElementById("cambio").value;
+    let resultado="";
+    resultado=valor4 / dePesoAlibra
+    alert(resultado)
+    }
+    libra();
+
+    
+
